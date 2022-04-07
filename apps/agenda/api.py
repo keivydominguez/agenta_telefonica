397ac405266ae6265e5239a658ff91c1976b8388
keivydominguez <keivysanchez@gmail.com>
2022-04-06 22:56:38 -0500

    Actualizacion v3

--- apps/agenda/api.py
+++ apps/agenda/api.py
@@ -10,23 +10,23 @@ class PhoneNumberAPIView(generics.ListCreateAPIView):
     serializer_class = PhoneNumberSerializer
 
 
-class PhoneNumberAPIViewPut(generics.CreateAPIView):
+class PhoneNumberAPIViewPut(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PhoneNumberSerializer
 
-    def get(self, request, pk=None):
-        qs = PhoneNumber.objects.filter(id=pk).first()
+    def retrieve(self, request, *args, **kwargs):
+        qs = PhoneNumber.objects.filter(pk=kwargs.get('pk')).first()
         serializer = self.get_serializer(qs)
         return Response(serializer.data)
 
-    def put(self, request, pk=None):
-        qs = PhoneNumber.objects.filter(id=pk).first()
+    def update(self, request, *args, **kwargs):
+        qs = PhoneNumber.objects.filter(pk=kwargs.get('pk')).first()
         serializer = self.get_serializer(qs, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    def delete(self, request, pk, format=None):
-        qs = PhoneNumber.objects.filter(id=pk).first()
+    def destroy(self, request, *args, **kwargs):
+        qs = PhoneNumber.objects.filter(pk=kwargs.get('pk')).first()
         qs.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -36,7 +36,7 @@ class PhonebookAPIView(generics.ListCreateAPIView):
     serializer_class = PhonebookSerializer
 
 
-class PhonebookAPIViewPut(generics.CreateAPIView):
+class PhonebookAPIViewPut(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PhonebookSerializer
 
     def get(self, request, pk=None):
